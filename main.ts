6738143479b4a3c4b9b44be603cef4520ea21172
ineshdeez <122534302+ineshdeez@users.noreply.github.com>
2023-02-09 13:55:27 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+radio.onReceivedString(function (receivedString) {
+    led.plot(parseFloat(receivedString.substr(0, 1)), parseFloat(receivedString.substr(1, 1)))
+})
+radio.setGroup(1)
